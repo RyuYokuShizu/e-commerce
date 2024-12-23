e36ff129ebec2d5b10da8da7116d0ecef8f8d392
Yotaro0819 <ay.araki0819@icloud.com>
2024-12-23 17:39:43 +0800

    Revert "created product/show file"
    This reverts commit dc85cac2a1035a03ee9b4daf515624d4ed629752.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Auth::routes();
-Route::group(['middleware' => 'auth'], function(){
-    Route::get('/', [HomeController::class, 'index'])->name('index');
-});
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('show', function () {
- return view('products.show');
-})->name('show');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
