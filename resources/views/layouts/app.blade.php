52075509fda1b0bd5b515b4d969b2938e93201de
Yotaro0819 <ay.araki0819@icloud.com>
2024-12-27 15:58:30 +0800

    latest push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,10 +61,6 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item">
-                                <a href="{{ route('cart.create')}}" class="">Create</a>
-                                 </li>
-                            <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
